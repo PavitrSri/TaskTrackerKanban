ad2ca78a5ebf6ea41b06bd78d2b0dee4cf03b7cd
Pavitra <pavitrasritamta@gmail.com>
2025-11-18 12:55:12 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import AddTask from "./components/AddTask"
+import ToDo from "./components/ToDo";
+function App() {
+  const [taskList, setTaskList] = useState([]);
+  return (
+   <>
+      <h1 className="text-2xl font-bold py-6 pl-6">Task Tracker</h1>
+      <p className="text-xl pl-6">Welcome!!</p>
+      <div className="flex flex-row items-center">
+      <p className="text-xl pl-6">Click</p>
+      <AddTask taskList={taskList} setTaskList={setTaskList}
+      /> 
+      <p className="text-xl my-2">to add a new task</p>
+      </div>
+      <div>
+        <h2 className="ml-6 text-xl font-semibold w-3/4 max-w-lg 
+        my-4 py-1 py-2 bg-gray-300">To Do:</h2>
+        
+        {taskList.slice(0).reverse().map((task, i) => 
+          < >
+            <ToDo
+              key={new Date().getTime()}
+              task={task}
+              index={i}
+              taskList={taskList}
+              setTaskList={setTaskList}
+            />
+            </>
+          )}
+      </div>
+   </>
+  );
+}
+
+export default App;
